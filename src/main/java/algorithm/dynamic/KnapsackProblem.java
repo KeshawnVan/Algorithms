0cc88b6b8bc92b4abc1007c5e25b163eaee17a05
Fankx <fankx@startimes.com.cn>
2019-01-14 11:33:30 +0800

    KnapsackProblem & DynamicProgram

--- src/main/java/algorithm/dynamic/KnapsackProblem.java
+++ src/main/java/algorithm/dynamic/KnapsackProblem.java
@@ -6,10 +6,10 @@ public class KnapsackProblem {
         boolean[][] states = new boolean[items.length][weight + 1];
         states[0][0] = true;
         states[0][items[0]] = true;
-        return calculate(items, states, weight);
+        return calculate(items, weight, states);
     }
 
-    private static int calculate(int[] items, boolean[][] states, int weight) {
+    private static int calculate(int[] items, int weight, boolean[][] states) {
 
         for (int i = 1; i < items.length; i++) {
             for (int j = 0; j <= weight; j++) {
@@ -26,9 +26,30 @@ public class KnapsackProblem {
         return 0;
     }
 
+    public static int advanceCalculate(int[] items, int weight) {
+        boolean[] states = new boolean[weight + 1];
+        states[0] = true;
+        states[items[0]] = true;
+        return advanceCalculate(items, weight, states);
+    }
+
+    public static int advanceCalculate(int[] items, int weight, boolean[] states) {
+        for (int i = 1; i < items.length; i++) {
+            for (int j = weight - items[i]; j >= 0; j--) {
+                if (states[j]) states[j + items[i]] = true;
+            }
+        }
+        for (int i = weight; i >= 0; i--) {
+            if (states[i]) return i;
+        }
+
+        return 0;
+    }
+
+
     public static void main(String[] args) {
         int[] items = new int[]{5, 7, 4, 5, 7, 3, 9, 4, 6};
-        int calculate = calculate(items, 49);
-        System.out.println(calculate);
+        System.out.println(calculate(items, 49));
+        System.out.println(advanceCalculate(items, 49));
     }
 }
