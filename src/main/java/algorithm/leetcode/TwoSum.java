8e21dfade57cdb38592ebc5358005a558b1a0179
Fankx <fankx@startimes.com.cn>
2019-02-25 11:05:49 +0800

    addTwoNumbers

--- src/main/java/algorithm/leetcode/TwoSum.java
+++ src/main/java/algorithm/leetcode/TwoSum.java
@@ -4,22 +4,21 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- * <p>
- * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
- * <p>
- * 示例:
- * <p>
- * 给定 nums = [2, 7, 11, 15], target = 9
- * <p>
- * 因为 nums[0] + nums[1] = 2 + 7 = 9
- * 所以返回 [0, 1]
- */
+;
+
+
 public class TwoSum {
 
     private Map<Integer, Integer> map = new HashMap<>();
 
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+     * 示例:
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     */
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
@@ -31,8 +30,63 @@ public class TwoSum {
         throw new IllegalArgumentException();
     }
 
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode header = new ListNode(0);
+        ListNode current = header;
+
+        while (l1 != null || l2 != null) {
+            int l1Val = l1 != null ? l1.val : 0;
+            int l2Val = l2 != null ? l2.val : 0;
+
+            int temp = l1Val + l2Val + carry;
+            carry = temp / 10;
+            current.next = new ListNode(temp % 10);
+            current = current.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        if (carry != 0) {
+            current.next = new ListNode(carry);
+        }
+        return header.next;
+    }
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
     public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
         int[] nums = new int[]{2, 7, 11, 15};
-        System.out.println(Arrays.toString(new TwoSum().twoSum(nums, 9)));
+        System.out.println(Arrays.toString(twoSum.twoSum(nums, 9)));
+
+        ListNode l1 = new ListNode(2);
+        ListNode l12 = new ListNode(4);
+        l1.next = l12;
+        l12.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        ListNode l22 = new ListNode(6);
+        l2.next = l22;
+        ListNode l23 = new ListNode(4);
+        l22.next = l23;
+        l23.next = new ListNode(1);
+
+//        ListNode l1 = new ListNode(5);
+//        ListNode l2 = new ListNode(5);
+        ListNode result = twoSum.addTwoNumbers(l1, l2);
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+
     }
 }
